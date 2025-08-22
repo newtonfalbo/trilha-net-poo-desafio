9feede2b614fa8df5bce3f12854d849e8ff239bc
Newton Falbo <newtonborges2022@gmail.com>
2025-08-22 15:28:27 -0300

    Implementação do desafio de POO - Smartphones (Nokia e iPhone)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("### Testando Nokia ###");
+        Smartphone nokia = new Nokia("1199999-0000", "Nokia Tijolão", "123456789", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n### Testando iPhone ###");
+        Smartphone iphone = new Iphone("1198888-1111", "iPhone 14 Pro", "987654321", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
